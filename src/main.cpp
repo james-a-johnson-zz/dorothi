831af629cf0bdfa99d844ed68d1c80f6d56bcd9f
James Johnson <jjohnsonjj1251@gmail.com>
2018-11-16 18:45:39 -0500

    Did a change

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,25 @@ int led2 = D7;
 
 void setup()
 {
-    //RGB.control(true);
-    /*
+    RGB.control(true);
     pinMode(led1, OUTPUT);
     pinMode(led2, OUTPUT);
-    */
     Serial.begin(9600);
 }
 
 void loop()
 {
-    Serial.print("A thing\n");
-    //RGB.color(255, 255, 255);
+    Serial.print("Michael sux\n");
 
-    //digitalWrite(led1, HIGH);
-    //digitalWrite(led2, HIGH);
+    RGB.color(255, 255, 255);
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
 
     delay(1000);
 
-    //digitalWrite(led1, LOW);
-    //digitalWrite(led2, LOW);
+    RGB.color(255, 0, 255);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
 
-    //delay(1000);
+    delay(1000);
 }
